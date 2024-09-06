803f5e8f31b933590f36a741b3817b45bd19b097
Adriel Ryan <adrielryan.tj@hotmail.com>
2024-09-06 14:05:01 -0300

    feat: Front - Adicionando função para deixar menu ativo

--- public/Site/js/script.js
+++ public/Site/js/script.js
@@ -41,3 +41,24 @@ const scrollUp = () =>{
                                             : scrollUp.classList.remove('show-scroll');
 }
 window.addEventListener('scroll', scrollUp);
+
+/*============= SHOW SCROLL UP =============*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () =>{
+    const scrollDown = window.scrollY
+
+    sections.forEach(current  =>{
+        const sectionHeight = current.offsetHeight,
+                sectionTop = current.offsetTop - 58,
+                sectionId = current.getAttribute('id'),
+                sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
+
+        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+            sectionClass.classList.add('active-link')
+        } else{
+            sectionClass.classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive);
