2168e0642dd2a6d1cbd1cdeb9d366dcc7e9dc42c
Adriel Ryan <adrielryan.tj@hotmail.com>
2024-09-06 13:28:09 -0300

    feat: Front - Finalizando estilizção do css

--- resources/views/Site/includes/footer.blade.php
+++ resources/views/Site/includes/footer.blade.php
@@ -1,5 +1,32 @@
 
-<!--========== SCROLL UP ==========-->
+<footer class="footer">
+    <div class="footer__container container grid">
+        <a href="#" class="footer__logo">Burguer</a>
+
+        <div class="footer__content grid">
+            <a href="#" class="footer__link">Termos e Políticas</a>
+            <a href="#" class="footer__link">Privacy Policy</a>
+
+            <div class="footer__social">
+                <a href="#" target="_blank">
+                    <i class="ri-facebook-circle-fill"></i>
+                </a>
+
+                <a href="#" target="_blank">
+                    <i class="ri-instagram-line"></i>
+                </a>
+
+                <a href="#" target="_blank">
+                    <i class="ri-whatsapp-line"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <span class="footer__copy">
+        &#169; Todos os Direitos Reservados por Burguer
+    </span>
+</footer>
 
 
 <!--=============== SCROLLREVEAL ===============-->
